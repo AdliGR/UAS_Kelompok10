37bcbdf2b35e135279952003b5273962e3772f9e
Shyehan Rafael (AdliGR) <82790794+AdliGR@users.noreply.github.com>
2024-05-11 12:09:07 +0700

    update Team.js

--- web_jawatimur/src/components/General.js
+++ web_jawatimur/src/components/General.js
@@ -8,7 +8,7 @@ function General() {
                     <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
                         <div className="service-item text-center pt-3">
                             <div className="p-4">
-                                <i className="fa fa-3x fa-graduation-cap text-primary mb-4"></i>
+                                <i className="fa fa-3x fa-globe text-primary mb-4"></i>
                                 <h5 className="mb-3">Pegunungan</h5>
                                 <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
                             </div>
--- web_jawatimur/src/components/Team.js
+++ web_jawatimur/src/components/Team.js
@@ -10,7 +10,7 @@ function Team() {
                 </div>
                 <div className="owl-carousel testimonial-carousel position-relative">
                     <div className="testimonial-item text-center">
-                        <img className="border rounded-circle p-2 mx-auto mb-3" src="img/testimonial-1.jpg" style={{ width: '80px', height: '80px' }} alt="" />
+                        <img className="border rounded-circle p-2 mx-auto mb-3" src="img/team1.jpg" style={{ width: '80px', height: '80px' }} alt="" />
                         <h5 className="mb-0">Sheyhan Rafael Adlinugroho</h5>
                         <p>Mahasiswa Informatika</p>
                     </div>
